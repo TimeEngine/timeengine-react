1bf8f809e01bab408513024a7a9c0854aac8f743
kenokabe <kenokabe@gmail.com>
2016-01-15 00:22:29 -0500

    commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # timeengine-react
 TimeEngine Extension for Stateless React programming without props and state
+
+See
+[http://timeengine.github.io](http://timeengine.github.io)
---
+++ timeengine-react.jsx
@@ -0,0 +1,38 @@
+(() => {
+  'use strict';
+
+  var React = require('react');
+  var __ = require('timeengine');
+
+  //***React state with life cycle is stateless sequence*****
+  const __Component = (__seqEl) => {
+
+    class SeqComponent extends React.Component {
+      constructor() {
+        super();
+        this.state = {
+          seqEl: __seqEl.t
+        };
+        var timeseq = __seqEl
+          .tMap((val) => {
+            this.setState({
+              seqEl: val
+            });
+          });
+      }
+      render() {
+        return (<span>{this.state.seqEl}</span>);
+      };
+    }
+
+    return (<SeqComponent/>);
+  };
+  //------------------
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = __Component;
+  } else {
+    window.__Component = __Component;
+  }
+
+})();
